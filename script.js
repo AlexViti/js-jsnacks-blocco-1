c34e67a7787721a5966cc52ea96cc450fdaddb4c
AlexViti <alexviti12@gmail.com>
2022-02-25 14:22:34 +0100

    Add snack 1.4

--- script.js
+++ script.js
@@ -50,15 +50,68 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
 
-let sum = 0;
+// let sum = 0;
 
-for (let i = 1; i <= 10; i++) {
-	let input = prompt(`inserisci ${i}° numero`);
+// for (let i = 1; i <= 10; i++) {
+// 	let input = prompt(`inserisci ${i}° numero`);
 
-	while (isNaN(input) || input == '') {
-		input = prompt('inserisci un numero');
+// 	while (isNaN(input) || input == '') {
+// 		input = prompt('inserisci un numero');
+// 	}
+// 	sum += parseInt(input);
+// }
+
+// alert('la somma dei numeri inseriti è: ' + sum);
+
+/*
+SNACK 1.4
+
+In un array sono contenuti i nomi degli invitati alla festa del grande
+Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+no alla festa.
+*/
+
+let guestArr = ['Nick Carraway','Jay Gatsby','Daisy Buchanan','Tom Buchanan','Jordan Baker','Myrtle Wilson','George Wilson','Owl Eyes','Klipspringer','Meyer Wolfsheim'];
+
+const mainHtml = document.querySelector('main');
+mainHtml.innerHTML += `
+<p>In un array sono contenuti i nomi degli invitati alla festa del grande
+Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+no alla festa.</p>
+<input type="text" name="guest" id="guest">
+<button class="check">Controlla</button>
+`
+const input = document.getElementById('guest');
+const btnCheck = document.querySelector('.check');
+
+let guestFound = false;
+let i;
+
+btnCheck.addEventListener('click', function() {
+	for (i = 0; i < guestArr.length && !guestFound; i++) {
+		if (guestArr[i].toLowerCase() == input.value.toLowerCase()) {
+			guestFound = true;
+		}
 	}
-	sum += parseInt(input);
-}
 
-alert('la somma dei numeri inseriti è: ' + sum);
+	if (guestFound) {
+		mainHtml.innerHTML += `<div>Good evening Mr. ${guestArr[--i]}. You can give me your coat, the party is this way. Thanks you for coming.</div>`
+	} else {
+		mainHtml.innerHTML += `<div>Sir, I'm terribly sorry but your name is not on the guest list. I'm afraid I'll have to ask you to leave.</div>`
+	}
+});
+
+
+/*
+SNACK 1.5
+
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+/*
+SNACK 1.6
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+*/
